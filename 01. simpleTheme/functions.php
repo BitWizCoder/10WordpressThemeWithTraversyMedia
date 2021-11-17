cb1b20911c72e057d33c6fa0115cf0ff05a108cb
mhmdNoman <mhmdnoman01@gmail.com>
2021-11-17 09:51:12 +0600

    excerpt, btn, blog imag

--- 01. simpleTheme/functions.php
+++ 01. simpleTheme/functions.php
@@ -1,12 +1,24 @@
 <?php
 
-function add_theme_scripts() {
-	wp_enqueue_style( 'style', get_stylesheet_uri('style.css') );
+  // Enqueue css and js
+  function add_theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri('style.css') );
 
-	wp_enqueue_script( 'test', get_template_directory_uri() . '/script.js', true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/script.js', true);
 
+    }
+    add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+  // Add support for fetured image
+  function theme_setup(){
+    add_theme_support('post-thumbnails');
   }
+  add_action('after_setup_theme', 'theme_setup');
 
-  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+  // Remove brackets and add ... on excerpts.
+  function new_excerpt_more( $more ) {
+      return ' ...';
+    }
+    add_filter('excerpt_more', 'new_excerpt_more');
 
 ?>
--- 01. simpleTheme/index.php
+++ 01. simpleTheme/index.php
@@ -6,9 +6,17 @@
             <article class="post">
                 <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
                 <div class="meta">
-                Created By <?php the_author(); ?> on <?php echo get_the_date(); ?>
+                Created By <a href="<?php the_permalink(); ?>"><?php the_author(); ?> </a>| <?php echo get_the_date(); ?>
                 </div>
+                <?php if(has_post_thumbnail()): ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                <?php endif; ?>
                 <?php the_excerpt() ?>
+                <a href="<?php the_permalink(); ?>" class='btn'>
+                Read More
+                </a>
             </article>
         <?php endwhile; ?>
         <?php else: ?>
